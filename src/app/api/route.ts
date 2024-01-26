7203c5622353470645be136582917f7a64b71917
alexrali <alexlino@gmail.com>
2024-01-26 11:40:16 -0600

    Commented out code for getting clusters from the database in route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,14 +3,15 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
     try {
-        const clusters = await prisma.clusters.findMany();
+        //todo: get the clusters from the database
+        // const clusters = await prisma.clusters.findMany();
 
-        const groupedClusters = clusters.reduce((grouped: { [key: string]: typeof clusters[0][] }, cluster) => {
-            (grouped[cluster.cluster_id] = grouped[cluster.cluster_id] || []).push(cluster);
-            return grouped;
-        }, {});
+        // const groupedClusters = clusters.reduce((grouped: { [key: string]: typeof clusters[0][] }, cluster) => {
+        //     (grouped[cluster.cluster_id] = grouped[cluster.cluster_id] || []).push(cluster);
+        //     return grouped;
+        // }, {});
 
-        return NextResponse.json({ clusters: groupedClusters });
+        // return NextResponse.json({ clusters: groupedClusters });
     } catch (error) {   
         console.log("error getting the clusters", error);
         return NextResponse.json({ error: 'Error getting the clusters' });
